fef80a41af8e671bca38a5743577dd81460e8d5d
John Carlson <yottzumm@gmail.com>
2018-02-10 14:43:44 -0500

    Added comments

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 export CLASSPATH="target/Chandu-1.0-SNAPSHOT.jar;javax.json-1.0.4.jar;javax.json-api-1.0.jar"
+
+# Generates "EXPRESS" froM JSON
 java net.coderextreme.X3DJSONLD HelloWorldProgramOutput.json
+
+# Programmically generates "EXPRESS" and XML
 java net.coderextreme.Event
