64521ca7b71b3a0fadeb303b2458877664720ae0
John Carlson <yottzumm@gmail.com>
2018-02-10 15:07:41 -0500

    Upgraded javax.json

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export CLASSPATH="target/Chandu-1.0-SNAPSHOT.jar;javax.json-1.0.4.jar;javax.json-api-1.0.jar;target/test-classes"
+export CLASSPATH="target/Chandu-1.0-SNAPSHOT.jar;javax.json-1.1.2.jar;javax.json-api-1.1.2.jar;target/test-classes"
 
 # Generates "EXPRESS" froM JSON
 java net.coderextreme.JSON2EXPRESS HelloWorldProgramOutput.json
