f9697eeabb4c5342b6bf46b80fb8b79deb5bff35
chandubaba <chandubabamusic@gmail.com>
2018-03-16 15:02:54 +0530

    Add files via upload

---
+++ ann/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: chandubaba
+ *
+ * Created on 21 December, 2016, 6:08 PM
+ */
+#include "ann.h"
+
+int main(int argc, char** argv) {
+    //kicks(); //uncomment to create all possible kick patterns, already done.
+
+    // snares(); //uncomment to create all possible snare patterns, already done.
+
+    // kicksxsnares(); //uncomment to mix snares and kicks, already done.
+    parseWords();//uncomment to create all types of words, already done.
+    Formsentence("come");
+    return 0;
+}
